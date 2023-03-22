533c6270e77a20ae327f9f21ff59383c9966ec6e
Moana Rivera Padilla <moana.padilla.0@gmail.com>
2023-03-21 23:07:04 -0500

    Creación de Login
    Se creo el apartado de Log in, se estilizó y se hizo la conexión. Falto conectarlo a la página de productos.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,13 @@
-import { Inter } from '@next/font/google'
-import Layout from '@/components/Layouts/Layout'
+import Layout from '../components/Layouts/Layout'
+import LoginPage from '../components/LoginPage'
 
 
-
-
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Home () {
+export default function Home() {
   return (
-    <Layout>
-      <h1> hola mundo</h1>
-    </Layout>
+    <>
+      <Layout/>
+      <LoginPage/>
+      <Layout/>
+    </>
   )
 }
