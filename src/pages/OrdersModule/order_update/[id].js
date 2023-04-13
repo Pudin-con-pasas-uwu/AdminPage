823adeae7f7f94ccc4d22df693d62062d866950a
Gerardo Casanova C <gerardojccastro@gmail.com>
2023-04-12 23:25:14 -0500

    Cambio de rutas y validación de token
    Se cambiaron las rutas de fetch
    Se agregaron las validaciones del token debido a que se eliminaron por un conflicto

--- src/pages/OrdersModule/order_update/[id].js
+++ src/pages/OrdersModule/order_update/[id].js
@@ -1,23 +1,38 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import React from 'react'
 import Layout from '@/components/Layouts/Layout'
-import { useRouter } from "next/router";
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
 import OrderUpdate from '../../../components/Orders/OrderUpdate'
 
 const order_update = (props) => {
 
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    const router = useRouter();
-    console.log(router);
-    const { id } = router.query;
+  const router = useRouter();
+  const { id } = router.query;
+  const [users, setUsers] = useState(null);
 
-    if (!props) {
-        router.reload();
-        return null;
+  useEffect(() => {
+    const token = localStorage.getItem('adminToken');
+    if (!token) {
+      router.push('/');
+    } else {
+      fetchUsers();
     }
+  }, [router]);
 
-    if (typeof window !== 'undefined') {
-        const token = sessionStorage.getItem("token");
-    };
+  const fetchUsers = async () => {
+    try {
+      const res = await fetch('https://ecommerunid.sistemasdelcaribe.com/all_users');
+      const data = await res.json();
+      setUsers(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  if (!users) {
+    return null;
+  }
 
   return (
 
@@ -29,7 +44,7 @@ const order_update = (props) => {
 }
 
 order_update.getInitialProps = async (ctx) => {
-    const URL = 'https://ecommerce-unid.000webhostapp.com/order_detail'
+    const URL = 'https://ecommerunid.sistemasdelcaribe.com/all_order_details'
         const res = await fetch(URL)
         const data = await res.json()
         return {
